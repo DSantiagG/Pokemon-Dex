acc96eada584e69480a1755c602e8d30c324abe2
David Santiago Giron Muñoz <98716905+DSantiagG@users.noreply.github.com>
2025-11-06 17:44:33 -0500

    Initial Commit

---
+++ Pokemon Dex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokemon Dex
+//
+//  Created by David Giron on 6/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokemon Dex/Pokemon_DexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_DexApp.swift
+//  Pokemon Dex
+//
+//  Created by David Giron on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_DexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
